601b36a02e1e26ce92d9e3f4ef266042a0853c6c
Saul <Saul@DESKTOP-I7OBM1P>
2024-04-16 15:07:39 -0700

    se termino la estructura basica de empleado

--- RushTablePOS/src/modelo/Empleado.java
+++ RushTablePOS/src/modelo/Empleado.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
 package modelo;
 
 /**
@@ -11,6 +14,48 @@ package modelo;
 public class Empleado {
     int id;
     int userCode;
-    string nomble
+    String nombre;
+    String apellido;
+    String NSS;
+    Autoridad autoridad;
+    double antiguedad;
+    int diasVacaciones;
+    double pagoQuincenal;
+    String rol;
+    private LocalDate horarioEntrada;
+    private LocalTime horaInicioJornada;
+    private LocalTime horaFinJornada;
+    private Duration tiempoTrabajoDia;
+    private Duration tiempoTrabajoQuincena;
     
+    public void iniciarJornada() {
+        // Verificar si ya se inició la jornada hoy
+        if (horaInicioJornada != null && horaFinJornada != null) {
+            LocalTime horaActual = LocalTime.now();
+            if (horaActual.isBefore(horaInicioJornada) || horaActual.isAfter(horaFinJornada)) {
+                System.out.println("La jornada no puede iniciarse en este momento.");
+                return;
+            }
+        }
+        System.out.println("Jornada iniciada.");
+        // Actualizar tiempo de inicio de jornada
+        horaInicioJornada = LocalTime.now();
+    }
+
+    public void terminarJornada() {
+        // Verificar si la jornada ya se inició hoy
+        if (horaInicioJornada == null) {
+            System.out.println("No se puede terminar la jornada, primero inicie la jornada.");
+            return;
+        }
+        // Actualizar tiempo de fin de jornada
+        horaFinJornada = LocalTime.now();
+        // Calcular la duración de la jornada
+        Duration duracionJornada = Duration.between(horaInicioJornada, horaFinJornada);
+        // Sumar la duración de la jornada al tiempo total de trabajo del día
+        tiempoTrabajoDia = tiempoTrabajoDia.plus(duracionJornada);
+        // Sumar la duración de la jornada al tiempo total de trabajo de la quincena
+        tiempoTrabajoQuincena = tiempoTrabajoQuincena.plus(duracionJornada);
+        System.out.println("Jornada terminada.");
+    }
 }
